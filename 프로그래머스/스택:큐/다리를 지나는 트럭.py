852d1fbcc0d4c4778877937dc360702fe95119b1
lee-yujinn <leeyj7988@naver.com>
2021-02-05 18:57:46 +0900

    [프로그래머스] 다리를 지나는 트럭

---
+++ 프로그래머스/스택:큐/다리를 지나는 트럭.py
@@ -0,0 +1,14 @@
+def solution(bridge_length, weight, truck_weights):
+    time = 0
+    bridge = [0] * bridge_length
+    
+    while len(bridge) != 0:
+        time += 1
+        bridge.pop(0)
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+        
+    return time
