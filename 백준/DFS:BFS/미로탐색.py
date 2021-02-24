8b79c0b54b9d962a2cc966ca07202cc16c8f2e0c
lee-yujinn <leeyj7988@naver.com>
2021-02-24 18:17:53 +0900

    [백준] 미로탐색

---
+++ 백준/DFS:BFS/미로탐색.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+n,m = map(int, input().split())
+graph = [list(map(int, input().strip())) for _ in range(n)]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    count = 0
+    queue = deque()
+    queue.append((x,y))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if nx<0 or ny<0 or nx>=n or ny>=m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y]+1
+                queue.append((nx,ny))
+            print(graph)
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
+      
